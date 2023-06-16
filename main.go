3faa89fdefee16284bc6b7586af9bd61008e6342
Denton Gentry <dgentry@tailscale.com>
2023-06-15 20:34:55 -0700

    cmd/quis-custodiet: Convert to GraphQL.
    Squadcast migration to New Schedules aka Schedules v2
    requires use of GraphQL instead of REST to fetch schedule
    information.
    
    Use GraphQL to get the list, and the new API v4 `who-is-oncall`
    endpoint to determine if someone is oncall.
    
    Updates https://github.com/tailscale/corp/issues/11541
    
    Change-Id: I512d2db2540b101a851bd26c03ffaa110a26a9de
    Signed-off-by: Denton Gentry <dgentry@tailscale.com>

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"tailscale.com/tsweb"
@@ -37,13 +38,12 @@ type AccessTokenDetails struct {
 	IssuedAt    int    `json:"issued_at"`
 }
 
-type OnCallResponse struct {
-	Data OnCallDetails `json:"data"`
+type OncallResponse struct {
+	Data []OncallData `json:"data"`
 }
 
-type OnCallDetails struct {
-	ShiftType string        `json:"shift_type"`
-	Users     []UserDetails `json:"users"`
+type OncallData struct {
+	Oncall []UserDetails `json:"oncall"`
 }
 
 type UserDetails struct {
@@ -53,24 +53,25 @@ type UserDetails struct {
 }
 
 type SchedulesResponse struct {
-	Data []SchedulesDetails `json:"data"`
+	Data SchedulesResponseData `json:"data"`
+}
+
+type SchedulesResponseData struct {
+	Schedules []SchedulesDetails `json:"schedules"`
 }
 
 type SchedulesDetails struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID int
 }
 
-type EventsResponse struct {
-	Data []EventDetails `json:"data"`
+type TeamsResponse struct {
+	Data []TeamsDetails `json:"data"`
 }
 
-type EventDetails struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	UserIDs   []string  `json:"user_ids"`
-	StartTime time.Time `json:"start_time"`
-	EndTime   time.Time `json:"end_time"`
+type TeamsDetails struct {
+	ID    string `json:"id"`
+	OrgID string `json:"organization_id"`
+	Name  string `json:"name"`
 }
 
 type squadcast struct {
@@ -130,30 +131,83 @@ func (s *squadcast) get(path, token string, response any) error {
 
 	return json.NewDecoder(resp.Body).Decode(&response)
 }
-func (s *squadcast) isOK(token string) bool {
+
+func (s *squadcast) post(path, token, body string, response any) error {
+	url := s.baseURL + path
+	req, err := http.NewRequest(httpm.POST, url, strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("%s responded %s", url, resp.Status)
+	}
+
+	return json.NewDecoder(resp.Body).Decode(&response)
+}
+
+func (s *squadcast) isOK(token, teamID string) bool {
 	if token == "" {
 		log.Printf("Got empty token")
 		return false
 	}
 
+	graphql := fmt.Sprintf(`{"query":"{schedules(filters:{teamID: \"%s\"}){teamID name ID orgID}}"}`, teamID)
+
 	var sq SchedulesResponse
-	if err := s.get("/v3/schedules", token, &sq); err != nil {
+	if err := s.post("/v3/graphql", token, graphql, &sq); err != nil {
 		log.Printf("ERROR: could not get schedules: %s", err)
 		return false
 	}
 
-	if len(sq.Data) == 0 {
+	if len(sq.Data.Schedules) == 0 {
 		log.Printf("Found no schedules: %+v", sq)
 		return false
 	}
 
 	log.Printf("Fetched a list of schedules: %+v", sq.Data)
 
-	for _, sch := range sq.Data {
-		if !s.SomeoneOncallNow(token, sch.ID) {
+	for _, sch := range sq.Data.Schedules {
+		if !s.someoneOncall(token, teamID, sch.ID, time.Time{}) {
 			return false
 		}
-		if !s.someoneOncallAt(token, sch.ID, s.now().Add(s.futureWindow)) {
+		if !s.someoneOncall(token, teamID, sch.ID, s.now().Add(s.futureWindow)) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (s *squadcast) pollTeams(token string) bool {
+	if token == "" {
+		log.Printf("Got empty token")
+		return false
+	}
+
+	var sq TeamsResponse
+	if err := s.get("/v3/teams", token, &sq); err != nil {
+		log.Printf("ERROR: could not get teams: %s", err)
+		return false
+	}
+
+	if len(sq.Data) == 0 {
+		log.Printf("Found no teams: %+v", sq)
+		return false
+	}
+
+	log.Printf("Fetched a list of teams: %+v", sq.Data)
+
+	for _, team := range sq.Data {
+		if !s.isOK(token, team.ID) {
 			return false
 		}
 	}
@@ -183,7 +237,7 @@ func (s *squadcast) poll() {
 			}
 		}
 
-		if s.isOK(token) {
+		if s.pollTeams(token) {
 			squadcastActive.Add(1)
 			squadcastOncall.Set(1)
 		} else {
@@ -194,39 +248,23 @@ func (s *squadcast) poll() {
 	}
 }
 
-func (s *squadcast) SomeoneOncallNow(token, scheduleID string) bool {
-	var oc OnCallResponse
-	if err := s.get(fmt.Sprintf("/v3/schedules/%s/on-call", scheduleID), token, &oc); err != nil {
-		log.Printf("ERROR: could not get oncall for schedule %s: %s", scheduleID, err)
+func (s *squadcast) someoneOncall(token, teamID string, scheduleID int, at time.Time) bool {
+	var oc OncallResponse
+	path := fmt.Sprintf("/v4/schedules/who-is-oncall?teamId=%s&scheduleID=%d", teamID, scheduleID)
+	if !at.IsZero() {
+		path += fmt.Sprintf("&time=%s", at.UTC().Format(time.RFC3339))
+	}
+	if err := s.get(path, token, &oc); err != nil {
+		log.Printf("ERROR: could not get oncall for schedule %d: %s", scheduleID, err)
 		return false
 
 	}
-	if len(oc.Data.Users) == 0 {
-		log.Printf("Nobody is currently oncall for schedule %s", scheduleID)
+	if len(oc.Data) == 0 || len(oc.Data[0].Oncall) == 0 {
+		log.Printf("Nobody is currently oncall for schedule %d", scheduleID)
 		return false
 	}
-	log.Printf("Someone is currently oncall for schedule %s: %+v", scheduleID, oc.Data.Users)
+	log.Printf("Someone is currently oncall for schedule %d: %+v", scheduleID, oc.Data[0].Oncall)
 	return true
-}
-
-func (s *squadcast) someoneOncallAt(token, scheduleID string, at time.Time) bool {
-	var ev EventsResponse
-	if err := s.get(fmt.Sprintf("/v3/schedules/%s/events", scheduleID), token, &ev); err != nil {
-		log.Printf("ERROR: could not get events for schedule %s: %s", scheduleID, err)
-		return false
-
-	}
-	for _, e := range ev.Data {
-		if len(e.UserIDs) == 0 {
-			continue
-		}
-		if e.StartTime.Before(at) && e.EndTime.After(at) {
-			log.Printf("Someone is oncall for schedule %s at %s: %s", scheduleID, at, e.UserIDs)
-			return true
-		}
-	}
-	log.Printf("Nobody is oncall for schedule %s at %s", scheduleID, at)
-	return false
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -10,86 +10,84 @@ import (
 const schedules = `{
 	"data": [
 	  {
-		"id": "000000000123456789abcdef",
+		"id": "0000a0acd3057ef84015245f",
 		"name": "Control oncall",
 		"slug": "control-oncall",
-		"organization_id": "100000000123456789abcdef",
+		"organization_id": "00006b69a19765b2c39da2ee",
 		"colour": "#0f61dd",
 		"description": "",
 		"owner": {
-		  "id": "200000000123456789abcdef",
+		  "id": "00006b69a19765b2c39da1f3",
 		  "type": "team"
 		},
 		"access_control": [],
 		"organization": {
-		  "id": "300000000123456789abcdef",
-		  "name": "Example, Inc",
-		  "slug": "example-inc"
+		  "id": "00006b69a19765b2c39da1ee",
+		  "name": "Tailscale, Inc",
+		  "slug": "tailscale-inc"
 		}
 	  },
 	  {
-		"id": "400000000123456789abcdef",
+		"id": "00007ed13fab4f7fedc44d42",
 		"name": "Rotation 2",
 		"slug": "rotation-2",
-		"organization_id": "300000000123456789abcdef",
+		"organization_id": "00006b69a19765b2c39da1ee",
 		"colour": "#0f61dd",
 		"description": "",
 		"owner": {
-		  "id": "200000000123456789abcdef",
+		  "id": "00006b69a19765b2c39da1f3",
 		  "type": "team"
 		},
 		"access_control": [],
 		"organization": {
-		  "id": "300000000123456789abcdef",
-		  "name": "Example, Inc",
-		  "slug": "example-inc"
+		  "id": "00006b69a19765b2c39da1ee",
+		  "name": "Tailscale, Inc",
+		  "slug": "tailscale-inc"
 		}
 	  }
 	]
   }`
 
 const oncallGood = `{
-	"data": {
-	  "shift_type": "Normal Shift",
-	  "users": [
+	"data": [{
+	  "oncall": [
 		{
-		  "id": "500000000123456789abcdef",
+		  "id": "00005186f329137034b38419",
 		  "first_name": "Awesome",
 		  "last_name": "Possum",
-		  "email": "possum@example.com",
+		  "email": "possum@tailscale.com",
 		  "contact": {
 			"dial_code": "+44",
 			"phone_number": "5551111111"
 		  }
 		}
 	  ]
-	}
+      }]
   }`
 
 const oncallBad = `{
-	"data": {
-	  "shift_type": "Normal Shift",
-	  "users": []
-	}
+	"data": [{
+	  "oncall": []
+      }]
   }`
 
 const eventsGood = `{
 	"data": [
 	  {
-		"id": "600000000123456789abcdef",
+		"id": "00007f473fab4f7fedc44d46",
 		"calendar_id": "000000000000000000000000",
 		"start_time": "2023-02-02T00:00:00Z",
 		"end_time": "2023-02-03T00:00:00Z",
 		"name": "Possum Test Shift",
 		"user_ids": [
-		  "500000000123456789abcdef"
+		  "00005186f329137034b38419"
 		],
-		"series_id": "700000000123456789abcdef",
+		"series_id": "00007f473fab4f7fedc44d45",
 		"squad_ids": [],
 		"is_override": false,
-		"schedule_id": "400000000123456789abcdef",
+		"schedule_id": "00007ed13fab4f7fedc44d42",
 		"calendar": {
-		  "id": "400000000123456789abcdef",
+		  "id": "00007ed13fab4f7fedc44d42",
 		  "name": "",
 		  "slug": ""
 		}
@@ -98,6 +96,23 @@ const eventsGood = `{
 
 const eventsBad = `{"data": [ ]}`
 
+const graphqlGood = `{
+  "data": {
+    "schedules": [
+      {
+        "teamID": "0000a0acd3057ef84015245f",
+        "name": "Schedule 1",
+        "ID": 1
+      },
+      {
+        "teamID": "0000a0acd3057ef84015245f",
+        "name": "Schedule 2",
+        "ID": 2
+      }
+    ]
+  }
+}`
+
 func TestSquadcast(t *testing.T) {
 	for _, tt := range []struct {
 		name      string
@@ -105,37 +120,58 @@ func TestSquadcast(t *testing.T) {
 		responses map[string]string // url -> json
 		wantOK    bool
 	}{
-		{"all responses 404", time.Time{}, map[string]string{}, false},
-		{"no schedules", time.Time{}, map[string]string{"/v3/schedules": `{"data": []}`}, false},
-		{"nobody oncall now", time.Date(2023, 02, 01, 12, 00, 00, 0, time.UTC), map[string]string{
-			"/v3/schedules": schedules,
-			"/v3/schedules/000000000123456789abcdef/on-call": oncallBad,
-			"/v3/schedules/000000000123456789abcdef/events":  eventsGood,
+		{"all_responses_404", time.Time{}, map[string]string{}, false},
+		{"no_schedules", time.Time{}, map[string]string{"/v3/schedules": `{"data": []}`}, false},
+		{"nobody_oncall_now", time.Date(2023, 02, 01, 12, 00, 00, 0, time.UTC), map[string]string{
+			"/v3/schedules":                                 schedules,
+			"/v4/schedules/who-is-oncall":                   oncallBad,
+			"/v3/schedules/0000a0acd3057ef84015245f/events": eventsGood,
 		}, false},
-		{"nobody oncall tomorrow", time.Date(2024, 12, 12, 12, 12, 12, 12, time.UTC), map[string]string{
+		{"nobody_oncall_tomorrow", time.Date(2024, 12, 12, 12, 12, 12, 12, time.UTC), map[string]string{
 			"/v3/schedules": schedules,
-			"/v3/schedules/000000000123456789abcdef/on-call": oncallGood,
-			"/v3/schedules/000000000123456789abcdef/events":  eventsGood,
+			"/v3/schedules/0000a0acd3057ef84015245f/events": eventsGood,
 		}, false},
-		{"no oncall shifts", time.Date(2024, 12, 12, 12, 12, 12, 12, time.UTC), map[string]string{
+		{"no_oncall_shifts", time.Date(2024, 12, 12, 12, 12, 12, 12, time.UTC), map[string]string{
 			"/v3/schedules": schedules,
-			"/v3/schedules/000000000123456789abcdef/on-call": oncallGood,
-			"/v3/schedules/000000000123456789abcdef/events":  eventsBad,
+			"/v3/schedules/0000a0acd3057ef84015245f/events": eventsBad,
 		}, false},
-		{"all good", time.Date(2023, 02, 01, 12, 00, 00, 0, time.UTC), map[string]string{
+		{"all_good", time.Date(2023, 02, 01, 12, 00, 00, 0, time.UTC), map[string]string{
 			"/v3/schedules": schedules,
-			"/v3/schedules/000000000123456789abcdef/on-call": oncallGood,
-			"/v3/schedules/000000000123456789abcdef/events":  eventsGood,
+			"/v3/schedules/0000a0acd3057ef84015245f/events": eventsGood,
 		}, true},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				tt.responses["/v3/graphql"] = graphqlGood
 				// Always return "good" responses for the second rotation, to avoid repeating this in tests.
-				tt.responses["/v3/schedules/400000000123456789abcdef/on-call"] = oncallGood
-				tt.responses["/v3/schedules/400000000123456789abcdef/events"] = eventsGood
+				tt.responses["/v3/schedules/00007ed13fab4f7fedc44d42/events"] = eventsGood
 				if r, ok := tt.responses[r.URL.Path]; ok {
 					w.Write([]byte(r))
 					return
+				}
+				if r.URL.Path == "/v4/schedules/who-is-oncall" {
+					q := r.URL.Query()
+					if q.Get("teamID") == "00007ed13fab4f7fedc44d42" {
+						// second rotation, return good
+						w.Write([]byte(oncallGood))
+						return
+					}
+					at := q.Get("at")
+					timenow := tt.now
+					var err error
+					if at != "" {
+						timenow, err = time.Parse(time.RFC3339, at)
+						if err != nil {
+							t.Errorf("time.Parse bad at= parameter: %q", err)
+						}
+					}
+					if timenow.Year() > 1970 && timenow.Year() < 2024 {
+						w.Write([]byte(oncallGood))
+						return
+					} else {
+						w.Write([]byte(oncallBad))
+						return
+					}
 				}
 				w.WriteHeader(404)
 			}))
@@ -146,7 +182,7 @@ func TestSquadcast(t *testing.T) {
 				now:          func() time.Time { return tt.now },
 				futureWindow: 24 * time.Hour,
 			}
-			if got := s.isOK("token"); got != tt.wantOK {
+			if got := s.isOK("token", "0000a0acd3057ef84015245f"); got != tt.wantOK {
 				t.Errorf("IsOk() = %v; want %v", got, tt.wantOK)
 			}
 		})
